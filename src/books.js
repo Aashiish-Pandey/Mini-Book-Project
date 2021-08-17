2544d90aaf804d7a1dc218fccdb7184bb938435a
Aashiish-Pandey <ash006pandey@gmail.com>
2021-08-17 13:48:03 +0530

    final commit

---
+++ src/books.js
@@ -0,0 +1,192 @@
+export const books = [
+  {
+    id: 1,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/516yRDtSdUL._AC_UL200_SR200,200_.jpg",
+    title: "Complete Classnotes Arithmetic (Bilingual)",
+    author: "Gagan Pratap Sir",
+  },
+
+  {
+    id: 2,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL200_SR200,200_.jpg",
+    title: "Atomic Habits:",
+    author: "James Clear ",
+  },
+  {
+    id: 3,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/516yRDtSdUL._AC_UL200_SR200,200_.jpg",
+    title: "Complete Classnotes Arithmetic (Bilingual)",
+    author: "Gagan Pratap Sir",
+  },
+  {
+    id: 1,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/516yRDtSdUL._AC_UL200_SR200,200_.jpg",
+    title: "Complete Classnotes Arithmetic (Bilingual)",
+    author: "Gagan Pratap Sir",
+  },
+
+  {
+    id: 2,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL200_SR200,200_.jpg",
+    title: "Atomic Habits:",
+    author: "James Clear ",
+  },
+  {
+    id: 3,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/516yRDtSdUL._AC_UL200_SR200,200_.jpg",
+    title: "Complete Classnotes Arithmetic (Bilingual)",
+    author: "Gagan Pratap Sir",
+  },
+  {
+    id: 1,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/516yRDtSdUL._AC_UL200_SR200,200_.jpg",
+    title: "Complete Classnotes Arithmetic (Bilingual)",
+    author: "Gagan Pratap Sir",
+  },
+
+  {
+    id: 2,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL200_SR200,200_.jpg",
+    title: "Atomic Habits:",
+    author: "James Clear ",
+  },
+  {
+    id: 3,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/516yRDtSdUL._AC_UL200_SR200,200_.jpg",
+    title: "Complete Classnotes Arithmetic (Bilingual)",
+    author: "Gagan Pratap Sir",
+  },
+  {
+    id: 1,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/516yRDtSdUL._AC_UL200_SR200,200_.jpg",
+    title: "Complete Classnotes Arithmetic (Bilingual)",
+    author: "Gagan Pratap Sir",
+  },
+
+  {
+    id: 2,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL200_SR200,200_.jpg",
+    title: "Atomic Habits:",
+    author: "James Clear ",
+  },
+  {
+    id: 3,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/516yRDtSdUL._AC_UL200_SR200,200_.jpg",
+    title: "Complete Classnotes Arithmetic (Bilingual)",
+    author: "Gagan Pratap Sir",
+  },
+  {
+    id: 1,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/516yRDtSdUL._AC_UL200_SR200,200_.jpg",
+    title: "Complete Classnotes Arithmetic (Bilingual)",
+    author: "Gagan Pratap Sir",
+  },
+
+  {
+    id: 2,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL200_SR200,200_.jpg",
+    title: "Atomic Habits:",
+    author: "James Clear ",
+  },
+  {
+    id: 3,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/516yRDtSdUL._AC_UL200_SR200,200_.jpg",
+    title: "Complete Classnotes Arithmetic (Bilingual)",
+    author: "Gagan Pratap Sir",
+  },
+  {
+    id: 1,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/516yRDtSdUL._AC_UL200_SR200,200_.jpg",
+    title: "Complete Classnotes Arithmetic (Bilingual)",
+    author: "Gagan Pratap Sir",
+  },
+
+  {
+    id: 2,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL200_SR200,200_.jpg",
+    title: "Atomic Habits:",
+    author: "James Clear ",
+  },
+  {
+    id: 3,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/516yRDtSdUL._AC_UL200_SR200,200_.jpg",
+    title: "Complete Classnotes Arithmetic (Bilingual)",
+    author: "Gagan Pratap Sir",
+  },
+  {
+    id: 1,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/516yRDtSdUL._AC_UL200_SR200,200_.jpg",
+    title: "Complete Classnotes Arithmetic (Bilingual)",
+    author: "Gagan Pratap Sir",
+  },
+
+  {
+    id: 2,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL200_SR200,200_.jpg",
+    title: "Atomic Habits:",
+    author: "James Clear ",
+  },
+  {
+    id: 3,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/516yRDtSdUL._AC_UL200_SR200,200_.jpg",
+    title: "Complete Classnotes Arithmetic (Bilingual)",
+    author: "Gagan Pratap Sir",
+  },
+  {
+    id: 1,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/516yRDtSdUL._AC_UL200_SR200,200_.jpg",
+    title: "Complete Classnotes Arithmetic (Bilingual)",
+    author: "Gagan Pratap Sir",
+  },
+
+  {
+    id: 2,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL200_SR200,200_.jpg",
+    title: "Atomic Habits:",
+    author: "James Clear ",
+  },
+  {
+    id: 3,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/516yRDtSdUL._AC_UL200_SR200,200_.jpg",
+    title: "Complete Classnotes Arithmetic (Bilingual)",
+    author: "Gagan Pratap Sir",
+  },
+  {
+    id: 1,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/516yRDtSdUL._AC_UL200_SR200,200_.jpg",
+    title: "Complete Classnotes Arithmetic (Bilingual)",
+    author: "Gagan Pratap Sir",
+  },
+
+  {
+    id: 2,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL200_SR200,200_.jpg",
+    title: "Atomic Habits:",
+    author: "James Clear ",
+  },
+  {
+    id: 3,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/516yRDtSdUL._AC_UL200_SR200,200_.jpg",
+    title: "Complete Classnotes Arithmetic (Bilingual)",
+    author: "Gagan Pratap Sir",
+  },
+  {
+    id: 1,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/516yRDtSdUL._AC_UL200_SR200,200_.jpg",
+    title: "Complete Classnotes Arithmetic (Bilingual)",
+    author: "Gagan Pratap Sir",
+  },
+
+  {
+    id: 2,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL200_SR200,200_.jpg",
+    title: "Atomic Habits:",
+    author: "James Clear ",
+  },
+  {
+    id: 3,
+    img: "https://images-eu.ssl-images-amazon.com/images/I/516yRDtSdUL._AC_UL200_SR200,200_.jpg",
+    title: "Complete Classnotes Arithmetic (Bilingual)",
+    author: "Gagan Pratap Sir",
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,35 +1,20 @@
 import React from "react";
 import ReactDom from "react-dom";
+import { books } from "./books";
+import Book from "./book";
 
-import './index.css'
+import "./index.css";
 
 function Booklist() {
   return (
-    <section className ="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+    <section className="booklist">
+      {books.map((book) => {
+        const { img, title, author } = book;
+
+        return <Book {...book} />;
+      })}
     </section>
   );
 }
-
-const Book = () => {
-  return (
-    <article className ="book">
-      <Image />
-      <Title />
-      <Author />
-    </article>
-  );
-};
-
-const Image = () => (
-  <img src="https://images-eu.ssl-images-amazon.com/images/I/516yRDtSdUL._AC_UL200_SR200,200_.jpg" />
-);
-
-const Title = () => <h1>Complete Classnotes Arithmetic (Bilingual)</h1>;
-
-const Author = () => <h1>Gagan Pratap Sir</h1>;
 
 ReactDom.render(<Booklist />, document.getElementById("root"));
